8d5727703da0c43ab1819c19236f7b9e19134416
Ruba <ruba@Rubas-MacBook-Pro.local>
2024-01-03 23:22:12 -0800

    added database url

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const pg = require('pg')
-const client = new pg.Client('postgres://localhost/thing_tracker')
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/thing_tracker')
+
 
 
 const seed = async () => {
